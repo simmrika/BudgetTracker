4982241f150ac59853f875d9554681710b11eab2
simmrika <lamasimika6789@gmail.com>
2024-10-22 10:29:43 +0545

    Layout

--- BudgetTRacker/Entities/User.cs
+++ BudgetTRacker/Entities/User.cs
@@ -1,9 +1,33 @@
-﻿namespace BudgetTRacker.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BudgetTRacker.Entities
 {
     public class User
     {
 
+        [Key]
+        public int UserId { get; set; }
 
+
+        [Required]
+        public required string firstname { get; set; }
+
+
+        [Required]
+        public required string lastname { get; set; }
+
+
+        [Required]
+        public required string email { get; set; }
+
+
+        [Required]
+        public required string phonenumber { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+
+        public byte[] PasswordSalt { get; set; }
 
 
     }
