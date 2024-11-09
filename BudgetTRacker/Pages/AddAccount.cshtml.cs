ee1e44a7f4a92eb6bcb97b4c306a67c87729c566
simmrika <lamasimika6789@gmail.com>
2024-11-09 17:16:19 +0545

    AddAccount, IncomeExpenses, My Profile

---
+++ BudgetTRacker/Pages/AddAccount.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BudgetTRacker.Pages
+{
+    public class AddAccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
