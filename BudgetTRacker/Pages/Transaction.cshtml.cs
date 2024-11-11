ffa3d5f2ebe3af9cba034c174b538cf4835f1c32
simmrika <lamasimika6789@gmail.com>
2024-11-11 14:20:12 +0545

    Transaction(Back)

--- BudgetTRacker/Pages/Transaction.cshtml.cs
+++ BudgetTRacker/Pages/Transaction.cshtml.cs
@@ -1,3 +1,4 @@
+using BudgetTRacker.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,21 @@ namespace BudgetTRacker.Pages
 {
     public class TransactionModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<IndexModel> _logger;
+        private readonly BankTransactionDataService _bankTransactionDataService;
+
+        [BindProperty]
+        public IEnumerable<TransactionDto> Transactions { get; private set; } = new List<TransactionDto>();
+
+        public TransactionModel(ILogger<IndexModel> logger, BankTransactionDataService bankTransactionDataService)
         {
+            _logger = logger;
+            _bankTransactionDataService = bankTransactionDataService;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Transactions = await _bankTransactionDataService.GetTransactionsByUserIdAsync(1);
         }
     }
 }
